e164a8489ca66a09ba6a5264cfd13dffc43ae2c1
Almerio Tsany <almerio.tsany@gmail.com>
2025-11-25 19:00:48 +0700

    delete kids&baby at footer

--- index.php
+++ index.php
@@ -481,8 +481,6 @@ $result = mysqli_query($conn, $query);
                     <ul class="list-unstyled">
                         <li><a href="#">MEN</a></li>
                         <li><a href="#">WOMEN</a></li>
-                        <li><a href="#">KIDS</a></li>
-                        <li><a href="#">BABY</a></li>
                     </ul>
                 </div>
                 <div class="col-md-2 mb-3">
